820d7ca8f00b64053cf7d1860eb69172c9a6f196
dhruvesh24 <patel.dhruveshpatel@gmail.com>
2018-08-30 22:25:18 -0500

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5auto.Startup))]
+namespace MVC5auto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
